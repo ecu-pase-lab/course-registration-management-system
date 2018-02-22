28328f71b4676ef95b211f89511ca38218468cbe
James Adams <jadams27@elon.edu>
2018-02-21 22:19:17 -0500

    Moved data for drop-downs into Mock data class, removed unneeded classes and switched to use Dictionary

--- CourseRegistrationManagementSystem/CourseRegistrationManagementSystem/Controllers/HomeController.cs
+++ CourseRegistrationManagementSystem/CourseRegistrationManagementSystem/Controllers/HomeController.cs
@@ -5,16 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CourseRegistrationManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CourseRegistrationManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private MockCRMSData mockCRMSData;
+
+        public HomeController()
+        {
+            mockCRMSData = new MockCRMSData();
+        }
+
         public IActionResult Index()
         {
-
-            List<Semester> semesters = PopulateSemesters();
-            ViewData["SemesterList"] = semesters;
+            ViewBag.Semesters = mockCRMSData.PopulateSemesters();
 
             return View();
         }
@@ -24,32 +30,23 @@ namespace CourseRegistrationManagementSystem.Controllers
         {
             Console.Out.WriteLine("Selected: " + selectedSemester);
 
-            List<Subject> subjects = PopulateSubjects();
-            ViewData["SubjectList"] = subjects;
+            ViewBag.Subjects = mockCRMSData.PopulateSubjects();
 
-            List<ScheduleType> scheduleTypes = PopulateScheduleTypes();
-            ViewData["ScheduleTypeList"] = scheduleTypes;
+            ViewBag.ScheduleTypes = mockCRMSData.PopulateScheduleTypes();
 
-            List<InstructionalMethod> instructionalMethods = PopulateInstructionalMethods();
-            ViewData["InstructionalMethodList"] = instructionalMethods;
+            ViewBag.InstructionalMethods = mockCRMSData.PopulateInstructionalMethods();
 
-            List<Campus> campus = PopulateCampus();
-            ViewData["CampusList"] = campus;
+            ViewBag.Campuses = mockCRMSData.PopulateCampuses();
 
-            List<CourseLevel> courseLevels = PopulateCourseLevels();
-            ViewData["CourseLevelList"] = courseLevels;
+            ViewBag.CourseLevels = mockCRMSData.PopulateCourseLevels();
 
-            List<TermDuration> termDurations = PopulateTermDurations();
-            ViewData["TermDurationList"] = termDurations;
+            ViewBag.TermDurations = mockCRMSData.PopulateTermDurations();
 
-            List<Instructor> instructors = PopulateInstructors();
-            ViewData["InstructorList"] = instructors;
+            ViewBag.Instructors = mockCRMSData.PopulateInstructors();
 
-            List<Session> sessions = PopulateSessions();
-            ViewData["SessionList"] = sessions;
+            ViewBag.Sessions = mockCRMSData.PopulateSessions();
 
-            List<CourseAttribute> courseAttributes = PopulateCourseAttributes();
-            ViewData["CourseAttributeList"] = courseAttributes;
+            ViewBag.CourseAttributes = mockCRMSData.PopulateCourseAttributes();
 
             return View();
         }
@@ -57,266 +54,9 @@ namespace CourseRegistrationManagementSystem.Controllers
         [HttpPost]
         public IActionResult CourseResults()
         {
-            ViewBag.CourseList = PopulateCourses();
+            ViewBag.Courses = mockCRMSData.PopulateCourses();
 
             return View();
-        }
-
-        private List<Semester> PopulateSemesters()
-        {
-            Semester semester1 = new Semester(1, "Spring 2018 (View Only)");
-            Semester semester2 = new Semester(2, "Spring-Medical 2018 (View Only)");
-
-            List<Semester> semesterList = new List<Semester>
-            {
-                semester1,
-                semester2
-            };
-            return semesterList;
-        }
-
-        private List<Subject> PopulateSubjects()
-        {
-            Subject subject1 = new Subject(1, "All");
-            Subject subject2 = new Subject(2, "Computer Science");
-            Subject subject3 = new Subject(3, "English");
-            Subject subject4 = new Subject(4, "Mathematics");
-            Subject subject5 = new Subject(5, "Physics");
-            Subject subject6 = new Subject(6, "Software Engineering");
-            Subject subject7 = new Subject(7, "Spanish");
-
-            List<Subject> subjectList = new List<Subject>
-            {
-                subject1,
-                subject2,
-                subject3,
-                subject4,
-                subject5,
-                subject6,
-                subject7
-            };
-            return subjectList;
-        }
-
-        private List<ScheduleType> PopulateScheduleTypes()
-        {
-            ScheduleType scheduleType1 = new ScheduleType(1, "All");
-            ScheduleType scheduleType2 = new ScheduleType(2, "Clinical");
-            ScheduleType scheduleType3 = new ScheduleType(3, "Colloquia");
-            ScheduleType scheduleType4 = new ScheduleType(4, "Honors (DO NOT USE)");
-
-            List<ScheduleType> scheduleTypeList = new List<ScheduleType>
-            {
-                scheduleType1,
-                scheduleType2,
-                scheduleType3,
-                scheduleType4
-            };
-            return scheduleTypeList;
-        }
-
-        private List<InstructionalMethod> PopulateInstructionalMethods()
-        {
-            InstructionalMethod instructionalMethod1 = new InstructionalMethod(1, "All");
-            InstructionalMethod instructionalMethod2 = new InstructionalMethod(2, "Face to Face");
-            InstructionalMethod instructionalMethod3 = new InstructionalMethod(3, "Face to Face Remote Site");
-            InstructionalMethod instructionalMethod4 = new InstructionalMethod(4, "Hybrid Primarily Face to Face");
-
-            List<InstructionalMethod> instructionalMethodList = new List<InstructionalMethod>
-            {
-                instructionalMethod1,
-                instructionalMethod2,
-                instructionalMethod3,
-                instructionalMethod4
-            };
-            return instructionalMethodList;
-        }
-
-
-        private List<Campus> PopulateCampus() {
-            Campus campus1 = new Campus(1, "All");
-            Campus campus2 = new Campus(2, "DE/Internet");
-            Campus campus3 = new Campus(3, "Main Campus");
-
-            List<Campus> campusList = new List<Campus>
-            {
-                campus1,
-                campus2,
-                campus3
-            };
-            return campusList;
-        }
-
-        private List<CourseLevel> PopulateCourseLevels()
-        {
-            CourseLevel courseLevel1 = new CourseLevel(1, "All");
-            CourseLevel courseLevel2 = new CourseLevel(2, "Dental");
-            CourseLevel courseLevel3 = new CourseLevel(3, "Graduate");
-            CourseLevel courseLevel4 = new CourseLevel(4, "Professional (Doctorate/CAS)");
-            CourseLevel courseLevel5 = new CourseLevel(5, "Undergraduate");
-
-            List<CourseLevel> courseLevelList = new List<CourseLevel>
-            {
-                courseLevel1,
-                courseLevel2,
-                courseLevel3,
-                courseLevel4,
-                courseLevel5
-            };
-            return courseLevelList;
-        }
-
-        private List<TermDuration> PopulateTermDurations()
-        {
-            TermDuration termDuration1 = new TermDuration(1, "All");
-            TermDuration termDuration2 = new TermDuration(2, "Full Term");
-
-            List<TermDuration> termDurationList = new List<TermDuration>
-            {
-                termDuration1,
-                termDuration2
-            };
-            return termDurationList;
-        }
-
-        private List<Instructor> PopulateInstructors()
-        {
-            Instructor instructor1 = new Instructor(1, "Ding, Junhua");
-            Instructor instructor2 = new Instructor(2, "Hills, Mark");
-            Instructor instructor3 = new Instructor(3, "Nassehzadeh-Tabrizi, Moha");
-            Instructor instructor4 = new Instructor(4, "Vilkomir, Sergiy");
-
-            List<Instructor> instructorList = new List<Instructor>
-            {
-                instructor1,
-                instructor2,
-                instructor3,
-                instructor4
-            };
-            return instructorList;
-        }
-
-        private List<Session> PopulateSessions()
-        {
-            Session session1 = new Session(1, "All");
-            Session session2 = new Session(2, "Field-Based Course Eval");
-            Session session3 = new Session(3, "Lab-Based Course Eval");
-            Session session4 = new Session(4, "Not to be Surveyed Course Eval");
-
-            List<Session> sessionList = new List<Session>
-            {
-                session1,
-                session2,
-                session3,
-                session4
-            };
-            return sessionList;
-        }
-
-        private List<CourseAttribute> PopulateCourseAttributes()
-        {
-            CourseAttribute courseAttribute1 = new CourseAttribute(1, "All");
-            CourseAttribute courseAttribute2 = new CourseAttribute(2, "AR-Special Music Fees");
-            CourseAttribute courseAttribute3 = new CourseAttribute(3, "Bassoon");
-            CourseAttribute courseAttribute4 = new CourseAttribute(4, "Cello");
-            CourseAttribute courseAttribute5 = new CourseAttribute(5, "Clarinet");
-
-            List<CourseAttribute> courseAttributeList = new List<CourseAttribute>
-            {
-                courseAttribute1,
-                courseAttribute2,
-                courseAttribute3,
-                courseAttribute4,
-                courseAttribute5
-            };
-            return courseAttributeList;
-        }
-
-        private List<Course> PopulateCourses()
-        {
-            Seat seat1 = new Seat();
-            seat1.Capacity = 30;
-            seat1.Actual = 8;
-            seat1.Remaining = seat1.Capacity - seat1.Actual;
-            seat1.WaitlistCapacity = 10;
-            seat1.WaitlistActual = 0;
-            seat1.WaitlistRemaining = seat1.WaitlistCapacity - seat1.WaitlistActual;
-
-            Course course1 = new Course
-            {
-                ID = 1,
-                CourseName = "Software Construction",
-                CourseRegistarCode = "32329",
-                CourseSubjectCode = "SENG 6245",
-                SectionNumber = "001",
-                CourseTerm = "Spring 2018",
-                RegistrationStartDate = new DateTime(2017, 11, 3),
-                RegistrationEndDate = new DateTime(2018, 1, 12),
-                ClassStartDate = new DateTime(2018, 1, 8),
-                ClassEndDate = new DateTime(2018, 5, 3),
-                ClassInstructionalMethod = "Face to Face",
-                CreditHours = 3,
-                InstructorName = "Mark Hills (P)",
-                ClassroomName = "Brewster Building 0B204",
-                CampusName = "Main Campus",
-                ClassDays = "Monday,Wednesday",
-                ClassTimes = "2:00 pm - 3:15 pm",
-                CourseSeat = seat1,
-                Prerequisites = "Graduate level SENG 6230 Minimum Grade of C or Graduate level CSCI 6230 Minimum Grade of C",
-                TextbookName = "Software Design",
-                TextbookPrice = 80.00,
-                CourseLevels = new List<string>{
-                    "Graduate",
-                    "Professional (Doctorate/CAS)"
-                }
-
-            };
-
-            Seat seat2 = new Seat();
-            seat2.Capacity = 30;
-            seat2.Actual = 11;
-            seat2.Remaining = seat2.Capacity - seat2.Actual;
-            seat2.WaitlistCapacity = 10;
-            seat2.WaitlistActual = 0;
-            seat2.WaitlistRemaining = seat2.WaitlistCapacity - seat2.WaitlistActual;
-
-            Course course2 = new Course
-            {
-                ID = 2,
-                CourseName = "Software Requirements Engineering",
-                CourseRegistarCode = "32332",
-                CourseSubjectCode = "SENG 6255",
-                SectionNumber = "601",
-                CourseTerm = "Spring 2018",
-                RegistrationStartDate = new DateTime(2017, 11, 3),
-                RegistrationEndDate = new DateTime(2018, 1, 12),
-                ClassStartDate = new DateTime(2018, 1, 8),
-                ClassEndDate = new DateTime(2018, 5, 3),
-                ClassInstructionalMethod = "Internet or World Wide Web",
-                CreditHours = 3,
-                InstructorName = "Sergiy Vilkomir (P)",
-                ClassroomName = "N/A",
-                CampusName = "De/Internet Campus",
-                ClassDays = "Monday,Wednesday",
-                ClassTimes = "12:30 pm - 1:45 pm",
-                CourseSeat = seat2,
-                Prerequisites = "",
-                TextbookName = "Software Requirements",
-                TextbookPrice = 120.00,
-                CourseLevels = new List<string>{
-                    "Graduate",
-                    "Professional (Doctorate/CAS)"
-                }
-
-            };
-
-            List<Course> courses = new List<Course>
-            {
-                course1,
-                course2
-            };
-
-            return courses;
         }
 
         public IActionResult About()
@@ -335,7 +75,7 @@ namespace CourseRegistrationManagementSystem.Controllers
 
         public IActionResult Schedule()
         {
-            List<Course> allCourses = PopulateCourses();
+            List<Course> allCourses = mockCRMSData.PopulateCourses();
 
             List<Course> mondayCourses = new List<Course>();
             List<Course> tuesdayCourses = new List<Course>();
---
+++ CourseRegistrationManagementSystem/CourseRegistrationManagementSystem/Models/MockCRMSData.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using CourseRegistrationManagementSystem.Models;
+
+namespace CourseRegistrationManagementSystem.Controllers
+{
+    public class MockCRMSData
+    {
+        public MockCRMSData()
+        {
+            
+        }
+
+        public Dictionary<int, string> PopulateSemesters()
+        {
+            Dictionary<int, string> semesters = new Dictionary<int, string>();
+
+            semesters.Add(1, "Spring 2018 (View Only)");
+            semesters.Add(2, "Spring-Medical 2018 (View Only)");
+
+            return semesters;
+        }
+
+        public Dictionary<int, string> PopulateSubjects()
+        {
+            Dictionary<int, string> subjects = new Dictionary<int, string>();
+
+            subjects.Add(1, "All");
+            subjects.Add(2, "Computer Science");
+            subjects.Add(3, "English");
+            subjects.Add(4, "Mathematics");
+            subjects.Add(5, "Physics");
+            subjects.Add(6, "Software Engineering");
+            subjects.Add(7, "Spanish");
+
+            return subjects;
+        }
+
+        public Dictionary<int, string> PopulateScheduleTypes()
+        {
+            Dictionary<int, string> scheduleTypes = new Dictionary<int, string>();
+
+            scheduleTypes.Add(1, "All");
+            scheduleTypes.Add(2, "Clinical");
+            scheduleTypes.Add(3, "Colloquia");
+            scheduleTypes.Add(4, "Honors (DO NOT USE)");
+
+            return scheduleTypes;
+        }
+
+        public Dictionary<int, string> PopulateInstructionalMethods()
+        {
+            Dictionary<int, string> instructionalMethods = new Dictionary<int, string>();
+
+            instructionalMethods.Add(1, "All");
+            instructionalMethods.Add(2, "Face to Face");
+            instructionalMethods.Add(3, "Face to Face Remote Site");
+            instructionalMethods.Add(4, "Hybrid Primarily Face to Face");
+
+            return instructionalMethods;
+        }
+
+
+        public Dictionary<int, string> PopulateCampuses()
+        {
+            Dictionary<int, string> campuses = new Dictionary<int, string>();
+
+            campuses.Add(1, "All");
+            campuses.Add(2, "DE/Internet");
+            campuses.Add(3, "Main Campus");
+
+            return campuses;
+        }
+
+        public Dictionary<int, string> PopulateCourseLevels()
+        {
+            Dictionary<int, string> courseLevels = new Dictionary<int, string>();
+
+            courseLevels.Add(1, "All");
+            courseLevels.Add(2, "Dental");
+            courseLevels.Add(3, "Graduate");
+            courseLevels.Add(4, "Professional (Doctorate/CAS)");
+            courseLevels.Add(5, "Undergraduate");
+
+            return courseLevels;
+        }
+
+        public Dictionary<int, string> PopulateTermDurations()
+        {
+            Dictionary<int, string> termDurations = new Dictionary<int, string>();
+
+            termDurations.Add(1, "All");
+            termDurations.Add(2, "Full Term");
+
+            return termDurations;
+        }
+
+        public Dictionary<int, string> PopulateInstructors()
+        {
+            Dictionary<int, string> instructors = new Dictionary<int, string>();
+
+            instructors.Add(1, "Ding, Junhua");
+            instructors.Add(2, "Hills, Mark");
+            instructors.Add(3, "Nassehzadeh-Tabrizi, Moha");
+            instructors.Add(4, "Vilkomir, Sergiy");
+
+            return instructors;
+        }
+
+        public Dictionary<int, string> PopulateSessions()
+        {
+            Dictionary<int, string> sessions = new Dictionary<int, string>();
+
+            sessions.Add(1, "All");
+            sessions.Add(2, "Field-Based Course Eval");
+            sessions.Add(3, "Lab-Based Course Eval");
+            sessions.Add(4, "Not to be Surveyed Course Eval");
+
+            return sessions;
+        }
+
+        public Dictionary<int, string> PopulateCourseAttributes()
+        {
+            Dictionary<int, string> courseAttributes = new Dictionary<int, string>();
+
+            courseAttributes.Add(1, "All");
+            courseAttributes.Add(2, "AR-Special Music Fees");
+            courseAttributes.Add(3, "Bassoon");
+            courseAttributes.Add(4, "Cello");
+            courseAttributes.Add(5, "Clarinet");
+
+            return courseAttributes;
+        }
+
+        public List<Course> PopulateCourses()
+        {
+            Seat seat1 = new Seat();
+            seat1.Capacity = 30;
+            seat1.Actual = 8;
+            seat1.Remaining = seat1.Capacity - seat1.Actual;
+            seat1.WaitlistCapacity = 10;
+            seat1.WaitlistActual = 0;
+            seat1.WaitlistRemaining = seat1.WaitlistCapacity - seat1.WaitlistActual;
+
+            Course course1 = new Course
+            {
+                ID = 1,
+                CourseName = "Software Construction",
+                CourseRegistarCode = "32329",
+                CourseSubjectCode = "SENG 6245",
+                SectionNumber = "001",
+                CourseTerm = "Spring 2018",
+                RegistrationStartDate = new DateTime(2017, 11, 3),
+                RegistrationEndDate = new DateTime(2018, 1, 12),
+                ClassStartDate = new DateTime(2018, 1, 8),
+                ClassEndDate = new DateTime(2018, 5, 3),
+                ClassInstructionalMethod = "Face to Face",
+                CreditHours = 3,
+                InstructorName = "Mark Hills (P)",
+                ClassroomName = "Brewster Building 0B204",
+                CampusName = "Main Campus",
+                ClassDays = "Monday,Wednesday",
+                ClassTimes = "2:00 pm - 3:15 pm",
+                CourseSeat = seat1,
+                Prerequisites = "Graduate level SENG 6230 Minimum Grade of C or Graduate level CSCI 6230 Minimum Grade of C",
+                TextbookName = "Software Design",
+                TextbookPrice = 80.00,
+                CourseLevels = new List<string>{
+                    "Graduate",
+                    "Professional (Doctorate/CAS)"
+                }
+
+            };
+
+            Seat seat2 = new Seat();
+            seat2.Capacity = 30;
+            seat2.Actual = 11;
+            seat2.Remaining = seat2.Capacity - seat2.Actual;
+            seat2.WaitlistCapacity = 10;
+            seat2.WaitlistActual = 0;
+            seat2.WaitlistRemaining = seat2.WaitlistCapacity - seat2.WaitlistActual;
+
+            Course course2 = new Course
+            {
+                ID = 2,
+                CourseName = "Software Requirements Engineering",
+                CourseRegistarCode = "32332",
+                CourseSubjectCode = "SENG 6255",
+                SectionNumber = "601",
+                CourseTerm = "Spring 2018",
+                RegistrationStartDate = new DateTime(2017, 11, 3),
+                RegistrationEndDate = new DateTime(2018, 1, 12),
+                ClassStartDate = new DateTime(2018, 1, 8),
+                ClassEndDate = new DateTime(2018, 5, 3),
+                ClassInstructionalMethod = "Internet or World Wide Web",
+                CreditHours = 3,
+                InstructorName = "Sergiy Vilkomir (P)",
+                ClassroomName = "N/A",
+                CampusName = "De/Internet Campus",
+                ClassDays = "Monday,Wednesday",
+                ClassTimes = "12:30 pm - 1:45 pm",
+                CourseSeat = seat2,
+                Prerequisites = "",
+                TextbookName = "Software Requirements",
+                TextbookPrice = 120.00,
+                CourseLevels = new List<string>{
+                    "Graduate",
+                    "Professional (Doctorate/CAS)"
+                }
+
+            };
+
+            List<Course> courses = new List<Course>
+            {
+                course1,
+                course2
+            };
+
+            return courses;
+        }
+    }
+}
