339c471a17385db45f1182212ee614d6be4700ac
James Adams <jadams27@elon.edu>
2018-03-10 14:21:48 -0500

    Adding and removing courses is functional from course results page and from schedule. Added jQuery UI dialog for removing on schedule page

--- CourseRegistrationManagementSystem/CourseRegistrationManagementSystem/wwwroot/js/site.js
+++ CourseRegistrationManagementSystem/CourseRegistrationManagementSystem/wwwroot/js/site.js
@@ -15,20 +15,73 @@
     $("#advancedCourseSearchButton").click(function(){
         $("#advancedCourseSearchGroup").toggle();
     });
-   
+
+    $("#dialog-confirm").dialog({
+      autoOpen: false,
+      modal: true
+    });
 });
 
+function addOrRemoveCourseFromScheduleOnResultsScreen(id){
+    if($("#classScheduleCheckbox_" + id).is(':checked')){
+        $.ajax({
+            url: '/Home/AddCourseToSchedule',
+            data: { 'courseId' : id },
+            type: "POST",
+            cache: false,
+            success: function (success) {
+                $("#scheduleText_" + id).html("Added to Schedule");
+                $("#scheduleText_" + id).css("color", "green");
+            },
+            error: function (error) {
+                $("#scheduleText_" + id).html("Error adding course to schedule");
+                $("#scheduleText_" + id).css("color", "red");
+            }
+        });
+    } else {
+        $.ajax({
+            url: '/Home/RemoveCourseFromSchedule',
+            data: { 'courseId' : id },
+            type: "POST",
+            cache: false,
+            success: function (success) {
+                $("#scheduleText_" + id).html("Removed from Schedule");
+                $("#scheduleText_" + id).css("color", "red");
+            },
+            error: function (error) {
+                $("#scheduleText_" + id).html("Error removing course from schedule");
+                $("#scheduleText_" + id).css("color", "red");
+            }
+        });
+    }
+}
+
 function deleteCourseFromScheduleOnScheduleScreen(id){
-    $.ajax({
-        url: '/Home/RemoveCourseFromSchedule',
-        data: { 'courseId' : id },
-        type: "POST",
-        cache: false,
-        success: function (success) {
-            location.reload();
+    $("#dialog-confirm").dialog({
+      resizable: false,
+      height: "auto",
+      width: 300,
+      autoOpen: true,
+      modal: true,
+      buttons: {
+        "Remove Course": function() {
+            $.ajax({
+                url: '/Home/RemoveCourseFromSchedule',
+                data: { 'courseId' : id },
+                type: "POST",
+                cache: false,
+                success: function (success) {
+                    location.reload();
+                },
+                error: function (error) {
+                    alert("Error deleting course from schedule");
+                }
+            });    
+            $(this).dialog("close");
         },
-        error: function (error) {
-            alert("Error deleting course from schedule");
+        Cancel: function() {
+          $(this).dialog("close");
         }
+      }
     });
 }
